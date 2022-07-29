f881244d8b70d1ffd0123ffe81594179f59ea4bf
jyumohfour <jsya2020@mymail.pomona.edu>
2022-07-28 19:25:45 -0500

    appending to array with context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import User from './User'
 
 export const UserContext = React.createContext();
@@ -6,14 +6,33 @@ export const UserContext = React.createContext();
 // using class means using state => can't use hooks (specifically usestate)
 function App()  {
   const [local, setLocal] = useState(0);
+  const [arr, setArr] = useState([]);
+
+
 
   function funkyThing (props) {
-    setLocal(props === 'Add' ? local + 1 : local -1);
+    setLocal(props.butt === 'Add' ? local + 1 : local -1);
+    setArr(props.butt === 'Add' ? prev => [...prev, props.title] : prev => prev.filter(x => x !== props.title));
   }
+
+  useEffect(() => {
+    console.log(arr);
+  }, [arr])
+
+  // const handleAllergies = e => {
+  //   const { value, checked } = e.target;
+  //   if (checked) {
+  //     // push selected value in list
+  //     setAllergies(prev => [...prev, value]);
+  //   } else {
+  //     // remove unchecked value from the list
+  //     setAllergies(prev => prev.filter(x => x !== value));
+  //   }
 
   // acts as replacement for class state
   let props = {
     local : local,
+    arr: arr,
     toggleLocal: funkyThing
   }
 
--- src/inside.js
+++ src/inside.js
@@ -3,17 +3,23 @@ import { UserContext } from './App'
 
 export default function User() {
     const [butt, setButt] = useState('Add');
+    const [title, setTitle] = useState(Math.floor(Math.random()*100));
 
     function handleChange(props) {
         setButt(butt === 'Add' ? 'Remove' : 'Add');
         console.log("we handled SHIT");
     }
 
+    let boys = {
+        butt: butt,
+        title: title
+    }
+
     return (
         <div>
             <UserContext.Consumer>
                 {(props) => (
-                    <button onClick ={() => {props.toggleLocal(butt); handleChange()}}>{butt}</button>
+                    <button onClick ={() => {props.toggleLocal(boys); handleChange()}}>{butt}</button>
                 )}
             </UserContext.Consumer>
         </div>
